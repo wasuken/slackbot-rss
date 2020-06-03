c05c08a92c46444764407e639ecf19739de65987
wasuken <wevorence@gmail.com>
2020-06-03 13:49:13 +0900

    hackernews title translate en -> ja.

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
-	"./nicoSearch"
-	"./sendSlack"
+	"encoding/json"
 	"fmt"
 	"github.com/BurntSushi/toml"
 	"github.com/mmcdole/gofeed"
+	"github.com/wasuken/slackbot-rss/nicoSearch"
+	"github.com/wasuken/slackbot-rss/sendSlack"
+	"io/ioutil"
+	"net/http"
+	"net/url"
 	"os"
 	"strings"
 	"time"
@@ -70,6 +74,39 @@ func postNicoSearch() {
 	slack := &sendSlack.SlackMsg{Name: "Hook君", Text: text, Channel: "voiceroid", Url: config.Hooks.Url}
 	slack.PostToHookUrl()
 }
+
+var DEEPL_URL string = "https://script.google.com/macros/s/AKfycbxpY_dGm4hJWrq3pcBdv7VokXcnoPXxtKYTz6YNtlQ9VOK59Zi9/exec"
+
+type DeepLResult struct {
+	Code int    `json:"code"`
+	Text string `json:"text"`
+}
+
+func deeplTranslate(text, src, target string) string {
+	values := url.Values{}
+	values.Add("text", text)
+	values.Add("source", src)
+	values.Add("target", target)
+	resp, err := http.Get(DEEPL_URL + "?" + values.Encode())
+
+	if err != nil {
+		panic("get error")
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic("body parse error")
+	}
+	var result DeepLResult
+	if err := json.Unmarshal(body, &result); err != nil {
+		panic("json parse error")
+	}
+	if result.Code == 400 {
+		panic("api response error")
+	}
+	return result.Text
+}
+
 func postRss() {
 	var config Config
 	_, err := toml.DecodeFile(loadFiles(DEFAULT_LOAD_FILES), &config)
@@ -84,7 +121,7 @@ func postRss() {
 		feed, _ := fp.ParseURL(url)
 		text += "## " + url + " ##\n"
 		for _, item := range feed.Items {
-			linkList = append(linkList, "<"+item.Link+"|"+item.Title+">")
+			linkList = append(linkList, "<"+item.Link+"|"+deeplTranslate(item.Title, "en", "ja")+">")
 		}
 		sep = int(len(linkList) / 2)
 		text = strings.Join(linkList[0:sep], "\n")
